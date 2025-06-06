14921db0fde06c55ba5cad1ffce1e2ba3e57b2c9
theido74 <147144073+theido74@users.noreply.github.com>
2025-06-06 12:04:58 +0200

    Add files via upload

--- password_checker.py
+++ password_checker.py
@@ -2,24 +2,27 @@ import re
 from math import log
 
 def entropy(password):
-    char_class = 0
-    if re.search(r'[a-z]', password): char_class += 1  # Classe pour les minuscules
-    if re.search(r'[A-Z]', password): char_class += 1  # Classe pour les majuscules
-    if re.search(r'[0-9]', password): char_class += 1  # Classe pour les chiffres
-    if re.search(r'[\W]', password): char_class += 1   # Classe pour les caractères spéciaux
     
-    # Calcul de l'entropie
-    return len(password) * log(char_class or 1, 2)
+    total_symbols = 0
+    if re.search(r'[a-z]', password): total_symbols += 26  
+    if re.search(r'[A-Z]', password): total_symbols += 26  
+    if re.search(r'[0-9]', password): total_symbols += 10  
+    if re.search(r'[\W]', password): total_symbols += 32   
+    if total_symbols == 0:
+        return 0
+
+    return len(password) * log(total_symbols, 2)
+
 
 def check_password_strength(password):
-    entropy_value = entropy(password)  # Calculez l'entropie
+    entropy_value = entropy(password)
     results = {
         'length': len(password) >= 12,
         'lower': bool(re.search(r'[a-z]', password)),
         'upper': bool(re.search(r'[A-Z]', password)),
         'digit': bool(re.search(r'[0-9]', password)),
         'special': bool(re.search(r'[\W]', password)),
-        'entropy': entropy_value,  # Stockez la valeur d'entropie
+        'entropy': entropy_value,  
         'common_pattern': not (
             re.search(r'password|123|admin|qwerty', password, re.IGNORECASE) or password.isnumeric()
         )
